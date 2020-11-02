5e034cd9c12aad100780b3dc020358b1b6c82f73
Mohib Ismail <imohib168@gmail.com>
2020-11-02 22:04:01 +0500

    Learned about Components, passing props and PropTypes in React

--- react-proptype/src/index.js
+++ react-proptype/src/index.js
@@ -1,17 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import './index.css'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// function Comment({ author, message, likes }) {
+//     return (
+//         <div>
+//             <div className="author">{author}</div>
+//             <div className="message">{message}</div>
+//             <div className="likes">
+//                 {likes > 0 ? likes : 'No'} likes
+//             </div>
+
+//         </div>
+//     )
+// }
+
+// Comment.propTypes = {
+//     author: PropTypes.string.isRequired,
+//     message: PropTypes.string.isRequired,
+//     likes: PropTypes.number
+// }
+
+
+// A custom validator to check that the passed prop is exactly length 3 (either a string or an array)
+
+function customValidator(props, propName, ComponentName) {
+    if (props[propName].length !== 3) {
+        return new Error(
+            `Invalid prop ${propName} Supplied to ${ComponentName} Length is not 3`
+        )
+    }
+}
+
+const CustomTest = ({ myCustomProp }) => {
+    return (
+        <div>
+            {myCustomProp}
+        </div>
+    )
+}
+
+CustomTest.propTypes = {
+    myCustomProp: customValidator
+}
+
+// ReactDOM.render(<CustomTest myCustomProp={[1, 2, 3]} />, document.getElementById("root"))
+ReactDOM.render(<CustomTest myCustomProp="abc" />, document.getElementById("root"))
+
+
+// ReactDOM.render(<Comment author='an_error' message="Something Strange" likes={15} />, document.getElementById("root"));
