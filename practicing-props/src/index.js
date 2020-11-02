8d54b2f6da16d704389c25c7f6336e7a21034632
Mohib Ismail <imohib168@gmail.com>
2020-11-02 10:57:49 +0500

    Practiced making Components by passing props

--- practicing-props/src/index.js
+++ practicing-props/src/index.js
@@ -1,17 +1,108 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import moment from 'moment'
+
+function Tweet({ tweet }) {
+    return (
+        <div className="tweet">
+            <Avatar hash={tweet.gravatar} /> <div className="content">
+                <Author author={tweet.author} />
+                <Time time={tweet.timestamp} />
+                <Message message={tweet.message} />
+                <div className="buttons">
+                    <ReplyButton />
+                    <RetweetButton count={tweet.retweets} />
+                    <LikeButton count={tweet.likes} />
+                    <MoreOptionsButton />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+function Avatar({ hash }) {
+    const url = `https://www.gravatar.com/avatar/${hash}`
+    return (
+        <img
+            src={url}
+            className="avatar"
+            alt="avatar" />
+    )
+}
+
+function Message({ message }) {
+    return (
+        <div className="message">
+            {message}
+        </div>
+    );
+}
+
+function Author({ author: { name, handle } }) {
+    return (
+        <span className="author">
+            <span className="name">{name}</span>
+            <span className="handle">@{handle}</span>
+        </span>
+    );
+}
+
+const Time = ({ time }) => (
+    <span className="time">{moment(time).fromNow()}</span>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const ReplyButton = () => (
+    <i className="fa fa-reply reply-button" />
+);
+
+function getRetweetCount(count) {
+    if (count > 0) {
+        return (
+            <span className="retweet-count">
+                {count}
+            </span>
+        )
+    } else {
+        return null;
+    }
+}
+
+const RetweetButton = ({ count }) => (
+    <span className="retweet-button">
+        <i className="fa fa-retweet" />
+        {getRetweetCount(count)}
+    </span>
+);
+
+const LikeButton = ({ count }) => (
+    <span className="like-button">
+        <i className="fa fa-heart" />
+        {count > 0 &&
+            <span className="like-count">
+                {count}
+            </span>}
+    </span>
+);
+
+
+const MoreOptionsButton = () => (
+    <i className="fa fa-ellipsis-h more-options-button" />
+);
+
+
+const testTweet = {
+    message: "Something about cats.",
+    gravatar: "xyz",
+    author: {
+        handle: "imohib168",
+        name: "Mohib Ismail"
+    },
+    likes: 2,
+    retweets: 17,
+    timestamp: "2020-09-30 21:24:37"
+};
+
+
+ReactDOM.render(<Tweet tweet={testTweet} />, document.querySelector('#root'));
